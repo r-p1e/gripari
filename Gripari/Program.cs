80cea9dc60e3af5f548c5975863496f017a1e682
r-p1e <r-p1e@protonmail.com>
2016-09-29 02:06:43 -0700

    Main enter place

--- Gripari/Program.cs
+++ Gripari/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hucksters.Gripari
+using Hucksters.Forvaret.Input;
+using Hucksters.Forvaret.Output;
+
+namespace Hucksters.Forvaret
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int number = 1;
+            Console.WriteLine(number);
+
+            var input1c = new OneC();
+            var outputWeb = new WebOut();
+
+            input1c.GotEventLog += outputWeb.OnEventLog;
+
+            input1c.run();
         }
     }
+    
 }
